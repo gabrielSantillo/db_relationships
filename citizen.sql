297c0def2f591694ef440b0456c0eecbc6fcc63e
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-09-13 19:01:28 -0600

    Fisished the knowledge check about relationship

---
+++ citizen.sql
@@ -0,0 +1,2 @@
+insert into citizen (name, address, phone_number, date_of_birth) values ('Gabriel', 'Street 123', '852334567', '1996-06-11'), ('Natalia', 'Street 13', '555334567', '1985-06-14');
+insert into tax_account (date_created, account_number) values ('2022-09-13', 865);
